735da5d2a66757847b6df73e6400aed432e35a23
gitivs <hemangithakur1810@gmail.com>
2023-01-23 15:21:55 -0500

    first commit

---
+++ src/test/Basic_TestNg.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Basic_TestNg {
+	@Test
+	public void Demo()
+	{
+		System.out.println("Hello");
+	}
+	
+	@Test
+	public void Demo1()
+	{
+		System.out.println("Bye");
+	}
+}
---
+++ src/test/Demo_TestNG.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.testng.annotations.Test;
+
+	public class Demo_TestNG {
+		@Test
+		public void Demo()
+		{
+			System.out.println("Test");
+		}
+}
